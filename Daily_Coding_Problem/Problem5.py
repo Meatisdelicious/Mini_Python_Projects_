ca362baf01da737df197a35198e189998daa3631
Meatisdelicious <ssquerel@gmail.com>
2022-12-07 22:50:29 +0100

    unsolved error, 'tuple' has no attribute 'start'

--- Daily_Coding_Problem/Problem5.py
+++ Daily_Coding_Problem/Problem5.py
@@ -16,23 +16,32 @@ class Time_table_management:
         # in the case we don't have any interval (single instead of pair)
         if not intervals:
             return 0
+
         sorted_intervals = sorted(intervals, key=lambda it: (it.start, it.end))
 
         room_count = 0
         heap = []
         # [(30, 75), (0, 50), (60, 150)]
         # -->
-        print([(i.start, i.end) for i in sorted_intervals])
+        # print([(i.start, i.end) for i in sorted_intervals])
         for inter in sorted_intervals:
             # iterates though the list
             # and decomposes the start.value and end.value
             start, end = inter.start, inter.end
+            # while heap
             while heap and heap[0] <= start:
                 heappop(heap)
             heappush(heap, end)
 
             # counts the pairs (time intervals)
             room_count = max(len(heap), room_count)
-            print(start, end, heap)
+            # print(start, end, heap)
         return room_count
 
+
+if __name__ == "__main__":
+    time_table_management = Time_table_management()
+
+    time_intervals = (30, 75), (0, 50), (60, 150)
+    print(type(time_intervals))
+    print(time_table_management.min_Meeting_Rooms(time_intervals))
