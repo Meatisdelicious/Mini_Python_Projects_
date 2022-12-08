5851976a20e3249a57f537edcd0534c71c17ee94
Meatisdelicious <ssquerel@gmail.com>
2022-12-08 15:45:18 +0100

    adding comments for full understanding

--- Daily_Coding_Problem/Problem6.py
+++ Daily_Coding_Problem/Problem6.py
@@ -6,3 +6,37 @@
 # return whether the brackets are balanced (well-formed).
 # For example, given the string "([])[]({})", you should return true.
 # Given the string "([)]" or "((()", you should return false.
+
+open_parenthesis = ["[", "{", "("]
+closed_parenthesis = ["]", "}", ")"]
+
+def check(myString):
+    # method using stacks
+    stack = []
+    # iterates through the string
+    for i in myString:
+        # chooses what type of parenthesis to take (open or closed)
+        # 1st case, puts in the stacks only open parenthesis
+        if i in open_parenthesis:
+            stack.append(i)
+        # 2nd case,
+        elif i in closed_parenthesis:
+            # gets the index of the closed parenthesis (it's position) in the myString
+            position = closed_parenthesis.index(i)
+            #
+            if stack and open_parenthesis[position] == stack[-1]:
+                stack.pop()
+            else:
+                return "Unbalanced"
+    if len(stack) == 0:
+        return "Balanced"
+    else:
+        return "Unbalanced"
+
+# to test :
+string1 = "{aoizxa[]{()}}"
+print(string1, "-", check(string1))
+string2 = "[{}{})(]"
+print(string2, "-", check(string2))
+string3 = "((()"
+print(string3, "-", check(string3))
