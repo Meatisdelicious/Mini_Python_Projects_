f85451e71fefbaa50339397110078d8f1c26126c
Meatisdelicious <ssquerel@gmail.com>
2022-12-12 16:57:50 +0100

    problem partially understood and solved

--- Daily_Coding_Problem/Problem7.py
+++ Daily_Coding_Problem/Problem7.py
@@ -10,26 +10,40 @@
 # have no digits and consists solely of alphabetic characters.
 # You can assume the string to be decoded is valid.
 
-def encode(message):
-  encoded_message = ""
-  i = 0
-  # Iteration through the message until the end of the message --> Hence the len(msg)-1 <-> to msg[-1]
-  while (i <= len(message) - 1):
-    count = 1
-    # getting the character index
-    ch = message[i]
-    j = i
-    while (j < len(message) - 1):
-      if (message[j] == message[j + 1]):
-        count = count + 1
-        j = j + 1
-      else:
-        break
-    encoded_message = encoded_message + str(count) + ch
-    i = j + 1
-  return encoded_message
+class Solution:
+    def encode(self, message):
+        encoded_message = ""
+        i = 0
+        # Iteration through the message until the end of the message --> Hence the len(msg)-1 <-> to msg[-1]
+        while i <= len(message) - 1:
+            count = 1
+            # getting the character index
+            ch = message[i]
+            j = i
+            while (j < len(message) - 1):
+                if (message[j] == message[j + 1]):
+                    count = count + 1
+                    j = j + 1
+                else:
+                    break
+            encoded_message = encoded_message + str(count) + ch
+            i = j + 1
+        return encoded_message
+
+    def decode(self, s):
+        output = ""
+        num = ""
+        for i in s:
+            if i.isalpha():
+                output += i * int(num)
+                num = ""
+            else:
+                num += i
+        return output
 
 
-# Provide different values for message and test your program
-encoded_message = encode("ABBBBCCCCCCCCAB")
-print(encoded_message)
+# Test program
+ob = Solution()
+message = "AAAABBBCCDAA"
+print("encoded message :", ob.encode(message))
+print("decoded message :", ob.decode(ob.encode(message)))
