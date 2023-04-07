e11c243488435b2e92be21bc92be9b019c0cd2b6
meatisdelicious <ssquerel@gmail.com>
2023-04-07 14:09:05 +0200

    adding solution, problem solution understoo 70%

--- Daily_Coding_Problem/Problem9.py
+++ Daily_Coding_Problem/Problem9.py
@@ -7,7 +7,7 @@
 # Given this matrix, a start coordinate, and an end coordinate, 
 # return the minimum number of steps required to reach the end coordinate from the start. 
 # If there is no possible path, then return null. 
-# You can move up, left, down, and right. 
+# You can move up, down, right, left. 
 # You cannot move through walls. You cannot wrap around the edges of the board.
 
 # For example, given the following board:
@@ -22,28 +22,88 @@
 # since we would need to go through (1, 2) because there is a 
 # wall everywhere else on the second row. 
 
+from collections import deque
+def is_valid_move(matrix, row, col):
+    """
+    Helper function to check if a given move is valid
+    """
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    if row < 0 or row >= num_rows or col < 0 or col >= num_cols:
+        return False
+    # Check if the element at the given row and col is equal to 0
+    if matrix[row][col] != 0:
+        return False
+    return True
+
+def min_steps_to_reach_end(matrix, start, end):
+    """
+    Function to find the minimum number of steps to reach from start to end coordinate on the given board
+    """
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    queue = deque([(start[0], start[1], 0)])  # Using a deque for BFS
+    visited = set([(start[0], start[1])])  # Keep track of visited coordinates
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # Possible directions to move
+
+    while queue:
+        row, col, steps = queue.popleft()
+        if (row, col) == end:
+            return steps  # Return the steps once end coordinate is reached
+
+        for direction in directions:
+            new_row, new_col = row + direction[0], col + direction[1]
+            if is_valid_move(matrix, new_row, new_col) and (new_row, new_col) not in visited:
+                queue.append((new_row, new_col, steps + 1))
+                visited.add((new_row, new_col))
+    return None  # If no path is found
+
+def check_possible_path(function):
+    if function is not None :
+        print(f"The minimum number of steps to reach the end is {function}.")
+    else:
+        print("There is no possible path to reach the end.")    
+
+# Example usage:
 matrix = [[0, 0, 0, 0], 
-          [1, 1, 1, 1], 
-          [0, 0, 0, 1], 
+          [1, 1, 0, 1], 
+          [0, 0, 0, 0], 
           [0, 0, 0, 0]]
-num_rows_matrix = len(matrix)
-num_cols_matrix = len(matrix[0])
+start = (3, 0)
+end = (0, 0)
+check_possible_path(min_steps_to_reach_end(matrix, start, end))
+
+
+
+
+#  First clue... not finished
+
 # Iterate through the matrix in reverse order
 # for i in range(start_value,stop_value,step_value)
-count=0
-for row in range(num_rows_matrix - 1, -1, -1):
-    print("---row---", row)
-    for element in range(num_cols_matrix - 1, -1, -1):
-        matrix_element = matrix[row][element]
-        print("ele before filter :",matrix_element)
+def path_in_matrix(matrix):
+    num_rows_matrix = len(matrix)
+    num_cols_matrix = len(matrix[0])
+    total_count=0
+    for row in range(num_rows_matrix - 1, -1, -1):
+        print("---------row-----------", row)
         row_count=0
-        if matrix_element == 0 :
-            count+=1
+        for element in range(num_cols_matrix - 1, -1, -1):
+            matrix_element = matrix[row][element]
+            print("ele before filter :",matrix_element)
+            total_count+=1
             row_count+=1
-            print(row_count)
-            skip_row = True
-            break
-    if skip_row == True:
-        print(" ele after ele",matrix_element)
-        continue
-print("number of steps :",count)
+            if matrix_element == 0 :
+                skip_row = True
+                break
+            matrix_count_1=0
+            if matrix_element==1 :
+                matrix_count_1+=1
+                if matrix_count_1 == 3:
+                    print("NUll")
+        print("--row_st_ct-- :",row_count)
+        if skip_row == True:
+            print("   ele after filt",matrix_element)
+            continue
+        else :
+            print("Null", False), False
+    print("\n Total_number_of_steps :",total_count)
