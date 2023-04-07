f27f2ea640f7443b68c108bbd1556b8df9a17f6a
meatisdelicious <ssquerel@gmail.com>
2023-04-07 12:17:30 +0200

    successfully skiped matrix row

--- Daily_Coding_Problem/Problem9.py
+++ Daily_Coding_Problem/Problem9.py
@@ -6,7 +6,8 @@
 # Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
 # Given this matrix, a start coordinate, and an end coordinate, 
 # return the minimum number of steps required to reach the end coordinate from the start. 
-# If there is no possible path, then return null. You can move up, left, down, and right. 
+# If there is no possible path, then return null. 
+# You can move up, left, down, and right. 
 # You cannot move through walls. You cannot wrap around the edges of the board.
 
 # For example, given the following board:
@@ -22,18 +23,27 @@
 # wall everywhere else on the second row. 
 
 matrix = [[0, 0, 0, 0], 
-          [1, 1, 0, 1], 
+          [1, 1, 1, 1], 
           [0, 0, 0, 1], 
           [0, 0, 0, 0]]
 num_rows_matrix = len(matrix)
 num_cols_matrix = len(matrix[0])
 # Iterate through the matrix in reverse order
 # for i in range(start_value,stop_value,step_value)
+count=0
 for row in range(num_rows_matrix - 1, -1, -1):
+    print("---row---", row)
     for element in range(num_cols_matrix - 1, -1, -1):
         matrix_element = matrix[row][element]
-        #print(matrix[row][element])
-        if matrix_element == 1 :
-            print("wall",matrix_element)
+        print("ele before filter :",matrix_element)
+        row_count=0
         if matrix_element == 0 :
-            print("path",matrix_element)
+            count+=1
+            row_count+=1
+            print(row_count)
+            skip_row = True
+            break
+    if skip_row == True:
+        print(" ele after ele",matrix_element)
+        continue
+print("number of steps :",count)
