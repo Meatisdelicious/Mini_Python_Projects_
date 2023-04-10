62db7b7219b8897b6beb1814dc45e075db97c230
meatisdelicious <ssquerel@gmail.com>
2023-04-10 21:55:18 +0200

    adding minior changes

--- SimpleCarGame/SimpleCarGame.py
+++ SimpleCarGame/SimpleCarGame.py
@@ -1,12 +1,12 @@
 from ursina import *
 
 app = Ursina()
-carmera.orthographic = True
+#carmera.orthographic = True
 camera.fov = 10
 
 car = Entity(
-    module='quad'
-    texture='assets\car'
+    module='quad',
+    texture='assets\car',
     collide='box',
     scale=(2,1),
     rotation_z=-90
