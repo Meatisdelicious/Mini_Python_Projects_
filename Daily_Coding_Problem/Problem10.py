85afdded09857da0126b5eed6d2d9bbfadccf894
meatisdelicious <ssquerel@gmail.com>
2023-04-12 18:47:41 +0200

    solved sort list and element in list incrementation

--- Daily_Coding_Problem/Problem10.py
+++ Daily_Coding_Problem/Problem10.py
@@ -10,10 +10,32 @@
 
 # For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
 
-# 2
-# 1.5
-# 2
-# 3.5
-# 2
-# 2
-# 2
+# 2     ---> 2
+# 1.5   ---> 2,1 --> 1,2
+# 2     ---> 2,1,5 --> 1,2,5
+# 3.5   ---> 2,1,5,7 --> 1,2,5,7 --> 2+5/2 = 3.5
+# 2     ---> 2,1,5,7,2 --> 1,2,2,5,7 --> = 2
+# 2     ---> 2,1,5,7,2,0 --> 0,1,2,2,5,7 --> = 2
+# 2     ---> 2,1,5,7,2,0,5 --> 0,1,2,2,5,5,7 --> = 2
+
+number_list = [2, 1, 5, 7, 2, 0, 5]
+def median_of_list(random_list):
+    temp_list=[]
+    for i in random_list:
+        temp_list.append(i)
+        sorted_temp_list = sorted(temp_list)
+        print(sorted_temp_list)
+
+        # if we only have one number
+        if len(sorted_temp_list) == 1:
+            print("first element of list",sorted_temp_list[0])
+
+        # if we have a list that has a pair lenght
+        if len(sorted_temp_list)%2 == 1:
+            print("pair list")
+        # if we have a list that has an impair lenght
+        if len(sorted_temp_list)%2 == 0 :
+            print("impair list")
+    return 
+
+median_of_list(number_list)
