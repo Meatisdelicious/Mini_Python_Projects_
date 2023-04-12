7f158ad8b01b09c273454f1016c945c973eb3060
meatisdelicious <ssquerel@gmail.com>
2023-04-12 19:05:14 +0200

    solved how to return impair element of impair list

--- Daily_Coding_Problem/Problem10.py
+++ Daily_Coding_Problem/Problem10.py
@@ -24,18 +24,22 @@ def median_of_list(random_list):
     for i in random_list:
         temp_list.append(i)
         sorted_temp_list = sorted(temp_list)
-        print(sorted_temp_list)
-
-        # if we only have one number
-        if len(sorted_temp_list) == 1:
-            print("first element of list",sorted_temp_list[0])
 
         # if we have a list that has a pair lenght
-        if len(sorted_temp_list)%2 == 1:
+        if len(sorted_temp_list)%2 == 0:
             print("pair list")
+            print(sorted_temp_list)
+
         # if we have a list that has an impair lenght
-        if len(sorted_temp_list)%2 == 0 :
+        if len(sorted_temp_list)%2 == 1 :
             print("impair list")
+            print(sorted_temp_list)
+            # -0.5 to make sure we don't get an float
+            middle_list_index = int( (len(sorted_temp_list)+1)/2 - 0.5)
+            # print("middle_list_index",middle_list_index)
+            print("---Mediane--- :", sorted_temp_list[middle_list_index])
+            
+
     return 
 
 median_of_list(number_list)
