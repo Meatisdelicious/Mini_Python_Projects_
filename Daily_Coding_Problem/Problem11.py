45556e134cc87dba8bfa1c244bb87e71e2b6d616
meatisdelicious <ssquerel@gmail.com>
2023-04-13 18:51:54 +0200

    understood problem

--- Daily_Coding_Problem/Problem11.py
+++ Daily_Coding_Problem/Problem11.py
@@ -5,3 +5,8 @@
 # This problem was asked by Amazon.
 # Given an array and a number k that's smaller than the length of the array,
 # rotate the array to the right k elements in-place.
+
+
+test_array =  [1,3,2,4,6]
+number_k = 2
+
