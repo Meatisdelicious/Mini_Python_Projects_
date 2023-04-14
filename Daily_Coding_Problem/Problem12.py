2644b74842fe6be06a9ed393e6fb64d3c050ef1c
meatisdelicious <ssquerel@gmail.com>
2023-04-14 19:04:24 +0200

    understood problem

--- Daily_Coding_Problem/Problem12.py
+++ Daily_Coding_Problem/Problem12.py
@@ -13,19 +13,19 @@
 def get_nth_sevenish_num(number: int) -> int:
     curr = 1
     curr_iteration = 1
+
     while curr < number:
         curr_iteration += 1
         curr += curr_iteration
 
     curr -= curr_iteration
     result = 7 ** (curr_iteration - 1)
+    
     curr_to_add = 1
-
     for _ in range(number - curr - 1):
         result += curr_to_add
         curr_to_add *= 7
     return result
-
 
 if __name__ == "__main__":
     print(get_nth_sevenish_num(1))  # 1 = 7 ^ 0
@@ -35,18 +35,15 @@ if __name__ == "__main__":
     print(get_nth_sevenish_num(5))  # 50 = 7 ^ 0 + 7 ^ 2
     print(get_nth_sevenish_num(6))  # 57 = 7 ^ 0 + 7 ^ 1 + 7 ^ 2
 
-    
+
 #  This doesn't work for shit...
 import math
 numb = 49
 def sevenish_number(number):
-
     if math.sqrt(number) % number :
         print("is a power of 7 :",math.sqrt(number))
-
     for i in range(0,number,1):
         temp1=number*i
-        
         for j in range(i,number,1):
             temp2=number*j + temp1
             if temp2 == math.sqrt(7):
@@ -54,5 +51,4 @@ def sevenish_number(number):
     else : 
         print("this number is not a power of 7 :", number)
     return 
-
 sevenish_number(numb)
