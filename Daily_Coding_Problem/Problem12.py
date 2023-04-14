823c26c218a50738d0646d0d598c76c8f44b07c5
meatisdelicious <ssquerel@gmail.com>
2023-04-14 18:43:37 +0200

    first case solved

--- Daily_Coding_Problem/Problem12.py
+++ Daily_Coding_Problem/Problem12.py
@@ -8,3 +8,18 @@
 # or the sum of unique powers of 7.
 # The first few sevenish numbers are 1, 7, 8, 49, and so on.
 # Create an algorithm to find the nth sevenish number.
+import math
+numb = 49
+def sevenish_number(number):
+
+    if math.sqrt(number) % number :
+        print("is a power of 7 :",math.sqrt(number))
+
+    if :
+        
+    
+    else : 
+        print("this number is not a power of 7 :", number)
+    return 
+
+sevenish_number(numb)
