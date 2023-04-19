e680be7008b2bb261e75afea5f82aba06a41aa5c
meatisdelicious <ssquerel@gmail.com>
2023-04-19 13:42:50 +0200

    first try failed, up solution, understood partially

--- Daily_Coding_Problem/Problem16.py
+++ Daily_Coding_Problem/Problem16.py
@@ -20,23 +20,44 @@
 # Output: 0
 # Explanation: In this case, no transactions are done and the max profit = 0.
 
+def maxProfit(prices):
+    buy = 0
+    sell = 1
+    max_profit = 0
+    while sell < len(prices):
+        if prices[sell] > prices[buy]:
+            profit = prices[sell] - prices[buy]
+            max_profit = max(profit, max_profit)
+        else:
+            buy = sell
+        sell = sell + 1
+    return max_profit
+
 prices = [7,1,5,3,6,4]
+print("max_profit :",maxProfit(prices))
+
+
+
+# Non working solution 
+prices = [7,1,5,3,6,4]
+def list_to_pair_list(list):
+    res = [((i), (i + 1) % len(list)) for i in range(len(list))]
+    return res
 
 def Best_time(price_list):
     profit_list=[]
     list_result=[]
     index_list = []
     for i in price_list:
-        print("i--",i)
         for j in range(1,len(price_list),1):
-            print("j----",price_list[j])
             profit_list.append(i-price_list[j])
             index_I = price_list.index(i)
             index_J = price_list[j]
             index_list.append(index_I)
             index_list.append(index_J)
-    print("index_list",index_list)       
-    print("max_profit =",max(profit_list))
+    # print("index_list",index_list)
+    # print("index_list",list_to_pair_list(index_list))
+    # print("len indexL",len(list_to_pair_list(index_list)))
+    # print("max_profit =",max(profit_list))
+    # print("len profitL",len(profit_list))
     return profit_list
-
-print(Best_time(prices))
