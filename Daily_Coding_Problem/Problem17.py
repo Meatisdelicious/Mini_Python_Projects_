7f99f0cadf2444c6c11bfacf47ac5b78089bd916
meatisdelicious <ssquerel@gmail.com>
2023-04-20 19:22:44 +0200

    problem fully understood and solved

--- Daily_Coding_Problem/Problem17.py
+++ Daily_Coding_Problem/Problem17.py
@@ -26,11 +26,21 @@
 # Since an empty string reads the same forward and backward, it is a palindrome.
 import re
 def Valid_palindrome(string):
-    pre_processed_string = re.sub(r'[^a-zA-Z0-9]', '', string)
-    print(pre_processed_string)
-    return 
+    temp_string = re.sub(r'[^a-zA-Z0-9]', '', string)
+    pre_processed_string=temp_string.lower()
+    string_to_list =[]
+    for i in pre_processed_string:
+        string_to_list.append(i)
+
+    string_to_list_reverse=string_to_list[::-1]
+    if string_to_list == string_to_list_reverse :
+        return True
+    else :
+        return False 
 
 s = "A man, a plan, a canal: Panama"
 s2= "race a car"
 s3= " "
 print(Valid_palindrome(s))
+print(Valid_palindrome(s2))
+print(Valid_palindrome(s3))
