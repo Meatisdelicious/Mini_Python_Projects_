a22ac79b7dc410f8a35b484f933ed7d322bdddfe
meatisdelicious <ssquerel@gmail.com>
2023-04-26 18:24:24 +0200

    Binary search solved and fully understood

--- Daily_Coding_Problem/Problem20.py
+++ Daily_Coding_Problem/Problem20.py
@@ -6,14 +6,29 @@
 
 # You must write an algorithm with O(log n) runtime complexity.
 
-
 # Example 1:
-
 # Input: nums = [-1,0,3,5,9,12], target = 9
 # Output: 4
 # Explanation: 9 exists in nums and its index is 4
-# Example 2:
 
+# Example 2:
 # Input: nums = [-1,0,3,5,9,12], target = 2
 # Output: -1
 # Explanation: 2 does not exist in nums so return -1
+
+
+def BinarySearch(nums, target):
+
+    for element in nums:
+        if element == target:
+            return nums.index(element)
+    else:
+        return -1
+
+
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+print(BinarySearch(nums, target))
+nums1 = [-1, 0, 3, 5, 9, 12]
+target1 = 2
+print(BinarySearch(nums1, target1))
