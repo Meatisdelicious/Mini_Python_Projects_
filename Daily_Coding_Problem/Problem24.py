598a97b45fc334ce062463e8c4c33e9c736a9444
meatisdelicious <ssquerel@gmail.com>
2023-04-28 17:19:55 +0200

    adding linked list problem

--- Daily_Coding_Problem/Problem24.py
+++ Daily_Coding_Problem/Problem24.py
@@ -1,4 +1,5 @@
 # 12 Linked List Cycle
+
 # Given head, the head of a linked list,
 # determine if the linked list has a cycle in it.
 
@@ -8,3 +9,47 @@
 # connected to. Note that pos is not passed as a parameter.
 
 # Return true if there is a cycle in the linked list. Otherwise, return false.
+
+# Example 1:
+# Input: head = [3,2,0,-4], pos = 1
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+# Example 2:
+# Input: head = [1,2], pos = 0
+# Output: true
+# Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+# Example 3:
+# Input: head = [1], pos = -1
+# Output: false
+# Explanation: There is no cycle in the linked list.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        try:
+            slow = head
+            fast = head.next
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
+
+
+head = [3, 2, 0, -4]
+pos = 1
+print(hasCycle(head, pos))
