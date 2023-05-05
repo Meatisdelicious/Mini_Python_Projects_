56c9dccc9bc4235cbc473b297b5212a016226f9a
meatisdelicious <ssquerel@gmail.com>
2023-05-05 09:47:03 +0200

    adding comments for understanding

--- Daily_Coding_Problem/Problem27.py
+++ Daily_Coding_Problem/Problem27.py
@@ -26,23 +26,24 @@ class Solution(object):
         for char in mag:
             if ran and char == ran[0]:
                 ran.pop(0)
+        # if ran exist... return false
         if ran:
             return False
-        else :
-            return True 
+        else:
+            return True
 
-    
+
 sol = Solution()
 
-ransomNote="a"
-magazine="b"
-print(sol.canConstruct(ransomNote,magazine))
-ransomNote1="aa"
-magazine1="ab"
-print(sol.canConstruct(ransomNote1,magazine1))
-ransomNote2="aa"
-magazine2="aab"
-print(sol.canConstruct(ransomNote2,magazine2))
-ransomNote3="aab"
-magazine3="baa"
-print(sol.canConstruct(ransomNote3,magazine3))      
+ransomNote = "a"
+magazine = "b"
+print(sol.canConstruct(ransomNote, magazine))
+ransomNote1 = "aa"
+magazine1 = "ab"
+print(sol.canConstruct(ransomNote1, magazine1))
+ransomNote2 = "aa"
+magazine2 = "aab"
+print(sol.canConstruct(ransomNote2, magazine2))
+ransomNote3 = "aab"
+magazine3 = "baa"
+print(sol.canConstruct(ransomNote3, magazine3))
