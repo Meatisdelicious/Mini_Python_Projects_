ba8979226a1cb0e78449a876d86bb24114bfb316
meatisdelicious <ssquerel@gmail.com>
2023-05-05 18:34:39 +0200

    adding new class

--- Daily_Coding_Problem/Problem11.py
+++ Daily_Coding_Problem/Problem11.py
@@ -7,18 +7,24 @@
 # rotate the array to the right k elements in-place.
 
 import numpy as np
-test_array =  [1,3,2,4,6]
+test_array = [1, 3, 2, 4, 6]
 number_k = 2
 
 # Simple way (using imports)
-def rotate_right_by_k(array,k):
-    print("original_list :",array)
-    rotated_list = np.roll(array,k)
-    return print("rotated_list :",rotated_list)
-rotate_right_by_k(test_array,number_k)
+
+
+def rotate_right_by_k(array, k):
+    print("original_list :", array)
+    rotated_list = np.roll(array, k)
+    return print("rotated_list :", rotated_list)
+
+
+rotate_right_by_k(test_array, number_k)
 
 # hard way (without imports)
-def rotate_right_k(array,k):
+
+
+def rotate_right_k(array, k):
     # +1 bcs it didn't show the same result as above lol...
     index_after_k = k+1
     rotated_list = []
@@ -27,5 +33,7 @@ def rotate_right_k(array,k):
     index_before_k = abs(k-len(array))
     for j in array[:index_before_k]:
         rotated_list.append(j)
-    return print("this is the way : --rotated-hard-list--",rotated_list)
-rotate_right_k(test_array,number_k)
+    return print("this is the way : --rotated-hard-list--", rotated_list)
+
+
+rotate_right_k(test_array, number_k)
--- Daily_Coding_Problem/Problem28.py
+++ Daily_Coding_Problem/Problem28.py
@@ -19,3 +19,10 @@
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
