05103f31821dbdd1eeff60b34e1a2e0813cfd22e
meatisdelicious <ssquerel@gmail.com>
2023-05-05 19:06:24 +0200

    problem solved, fibonacci sequence, partially understood

--- Daily_Coding_Problem/Problem28.py
+++ Daily_Coding_Problem/Problem28.py
@@ -22,7 +22,12 @@
 
 class Solution(object):
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        ways = [1, 1, 2]
+        for step in range(3, n+1):
+            ways.append(ways[step-1]+ways[step-2])
+        return ways[n]
+
+
+obj = Solution()
+n = 2
+print(obj.climbStairs(n))
