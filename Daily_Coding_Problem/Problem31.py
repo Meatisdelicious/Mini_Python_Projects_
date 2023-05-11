de5f2aa3f0c00c126af0ccaf03d49aa993b946f8
meatisdelicious <ssquerel@gmail.com>
2023-05-11 10:36:08 +0200

    mods

--- Daily_Coding_Problem/Problem31.py
+++ Daily_Coding_Problem/Problem31.py
@@ -5,10 +5,9 @@
 # You may assume that the majority element always exists in the array.
 
 # Example 1:
-
 # Input: nums = [3,2,3]
 # Output: 3
-# Example 2:
 
+# Example 2:
 # Input: nums = [2,2,1,1,1,2,2]
 # Output: 2
