5438317d0996c2149005e973a7e4f4b07447913d
meatisdelicious <ssquerel@gmail.com>
2023-05-12 17:41:27 +0200

    solved but isn't efficient enought for leetcode

--- Daily_Coding_Problem/Problem32.py
+++ Daily_Coding_Problem/Problem32.py
@@ -12,8 +12,24 @@
 
 class Solution(object):
     def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
+        res = ""
+        carry = 0
+        # reverse input strings
+        a, b = a[::-1], b[::-1]
+
+        for i in range(max(len(a), len(b))):
+            digitA = ord(a[i]) - ord("O") if i < len(a) else 0
+            digitB = b[i] if i < len(b) else 0
+
+            total = digitA + digitB + carry
+            char = str(total % 2)
+            res = char + res
+            carry = total // 2
+        # if carry is non 0
+        if carry:
+            res = "1" + res
+        return res
+
+
+sol = Solution()
+print(sol.addBinary())
