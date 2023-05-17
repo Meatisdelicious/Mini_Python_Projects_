097ce579121f887999eb485de95ff9c015d9ed62
meatisdelicious <ssquerel@gmail.com>
2023-05-17 17:46:40 +0200

    implemented the solution

--- Daily_Coding_Problem/Problem36.py
+++ Daily_Coding_Problem/Problem36.py
@@ -18,7 +18,27 @@
 
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+        seen = set()
+        duplicates = []
+        count = 0
+        for i in nums:
+            if i in seen:
+                duplicates.append(i)
+                count += 1
+            else:
+                seen.add(i)
+        print(seen, "---", duplicates, "---", count)
+
+        if count > 0:
+            return True, print(True)
+        else:
+            return False, print(False)
+
+
+sol = Solution()
+nums1 = [1, 2, 3, 1]
+nums2 = [1, 2, 3, 4]
+nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+sol.containsDuplicate(nums1)
+sol.containsDuplicate(nums2)
+sol.containsDuplicate(nums3)
