83ae194c74b8576a0355da804097ad9c02b866bf
meatisdelicious <ssquerel@gmail.com>
2023-05-22 18:24:24 +0200

    adding expt case

--- Daily_Coding_Problem/Problem37.py
+++ Daily_Coding_Problem/Problem37.py
@@ -47,16 +47,10 @@ class Solution(object):
     def romanToInt(self, s):
         roman_map = {'I': 1, 'V': 5, 'X': 10,
                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0        
+        for i in range(len(s)):
+            if i+1 <len(s) and roman_map[s[i]] < roman_map(s[i+1]):
 
-        list = []
-        for i in s:
-            if i in roman_map:
-
-                value = roman_map.get(i)
-                list.append(value)
-
-        print("list", list)
-        result = sum(list)
         return
 
 
