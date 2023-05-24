5154414942abdcbc4f97734af197fffc4ae04e36
meatisdelicious <ssquerel@gmail.com>
2023-05-24 18:30:41 +0200

    problem partially solved, needs revision

--- Daily_Coding_Problem/Problem39.py
+++ Daily_Coding_Problem/Problem39.py
@@ -1,21 +1,18 @@
 # w3 6 338. Counting Bits   (6 bcs some were skipped)
 
-
 # Given an integer n, return an array ans of length n + 1
 # such that for each i (0 <= i <= n), ans[i] is the number
 # of 1's in the binary representation of i.
 
-
 # Example 1:
-
 # Input: n = 2
 # Output: [0,1,1]
 # Explanation:
 # 0 --> 0
 # 1 --> 1
 # 2 --> 10
-# Example 2:
 
+# Example 2:
 # Input: n = 5
 # Output: [0,1,1,2,1,2]
 # Explanation:
@@ -28,11 +25,13 @@
 
 class Solution(object):
     def countBits(self, n):
-
-        return
+        output = [0]
+        while (len(output) <= n):
+            output.extend([i+1 for i in output])
+        return output[:n+1]
 
 
 sol = Solution()
 n = 2
 n1 = 5
-sol.countBits(n)
+print(sol.countBits(n1))
