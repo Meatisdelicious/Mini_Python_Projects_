ddefcba9401df96247e1e845346fd5bc4d0e0ae2
meatisdelicious <ssquerel@gmail.com>
2023-05-25 18:42:14 +0200

    adding non working solution, but understood problem, sort of

--- Daily_Coding_Problem/Problem40.py
+++ Daily_Coding_Problem/Problem40.py
@@ -27,12 +27,12 @@
 #         self.right = right
 class Solution(object):
     def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        return
+        # Cas de base
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.tight))
 
 
 sol = Solution()
