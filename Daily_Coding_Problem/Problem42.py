a2f2a5bfd578c009808759ebec991e7160df8ca9
meatisdelicious <ssquerel@gmail.com>
2023-05-31 18:25:40 +0200

    adding first clue

--- Daily_Coding_Problem/Problem42.py
+++ Daily_Coding_Problem/Problem42.py
@@ -1,5 +1,9 @@
 # w3 9 14. Longest Common Prefix
 
+# Write a function to find the longest common prefix
+# string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
 # Example 1:
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
@@ -9,10 +13,17 @@
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
 
-
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+        result = []
+        for i in range(len(strs)):
+            for j in range(i + 1, len(strs)):
+                common_elements = list(set(strs[i]) & set(strs[j]))
+                result.extend(common_elements)
+        return result
+
+
+strs1 = ["flower", "flow", "flight"]
+strs2 = ["dog", "racecar", "car"]
+sol = Solution()
+print(sol.longestCommonPrefix(strs2))
