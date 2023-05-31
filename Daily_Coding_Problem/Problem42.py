0cf20a40a757340c31a7ef41785c7a8f98d26339
meatisdelicious <ssquerel@gmail.com>
2023-05-31 11:25:40 +0200

    adding new class and problem

---
+++ Daily_Coding_Problem/Problem42.py
@@ -0,0 +1,18 @@
+# w3 9 14. Longest Common Prefix
+
+# Example 1:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+# Example 2:
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
