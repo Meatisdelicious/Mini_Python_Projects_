40b0b66d6841d222e45ff6d03228e9ff6459c126
meatisdelicious <ssquerel@gmail.com>
2023-06-02 18:08:39 +0200

    problem solved, need work

--- Daily_Coding_Problem/problem44.py
+++ Daily_Coding_Problem/problem44.py
@@ -11,4 +11,25 @@
 
 # Example 2:
 # Input: head = [1,2]
-# Output: false
+# Output: falsef
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        nums = []
+
+        while head:
+            nums.append(head.val)
+            head = head.next
+
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] != nums[r]:
+                return False
+
+        return True
