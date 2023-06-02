b53575bfe0dc92476a64c4b73d6eb58270c2b42c
meatisdelicious <ssquerel@gmail.com>
2023-06-02 17:29:25 +0200

    adding new problem

--- Daily_Coding_Problem/problem44.py
+++ Daily_Coding_Problem/problem44.py
@@ -1,20 +1,14 @@
-# w3 11 136. Single Number
+# w3 11 234. Palindrome Linked List
 
-# Given a non-empty array of integers nums, every
-# element appears twice except for one. Find that single one.
 
-# You must implement a solution with a linear
-# runtime complexity and use only constant extra space.
-
+# Given the head of a singly linked list, return true if it is a
+# palindrome
+#  or false otherwise.
 
 # Example 1:
-# Input: nums = [2,2,1]
-# Output: 1
+# Input: head = [1,2,2,1]
+# Output: true
 
 # Example 2:
-# Input: nums = [4,1,2,1,2]
-# Output: 4
-
-# Example 3:
-# Input: nums = [1]
-# Output: 1
+# Input: head = [1,2]
+# Output: false
