acce5a6e83f81395fe1f6a7a4ea8d3f910ed5b4b
meatisdelicious <ssquerel@gmail.com>
2023-06-06 18:25:19 +0200

    not understood yet, need work

--- Daily_Coding_Problem/Problem46.py
+++ Daily_Coding_Problem/Problem46.py
@@ -1,4 +1,4 @@
-# w4 101. Symmetric Tree
+# w4 1 101. Symmetric Tree
 
 # Given the root of a binary tree, check whether it
 # is a mirror of itself (i.e., symmetric around its center).
@@ -10,3 +10,20 @@
 # Example 2:
 # Input: root = [1,2,2,null,3,null,3]
 # Output: false
+
+class Solution(object):
+    def isSymmetric(self, root):
+        def dfs(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            (left.val == right.val and dfs(left.left, right.right)
+             and dfs(left.right, right.left))
+        return dfs(root.left, root.right)
+
+
+root1 = [1, 2, 2, 3, 4, 4, 3]
+root2 = [1, 2, 2, None, 3, None, 3]
+sol = Solution()
+sol.isSymmetric(root1)
