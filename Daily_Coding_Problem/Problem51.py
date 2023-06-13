4a7f7b394210bb846812e48b93934ab6521bb68e
meatisdelicious <ssquerel@gmail.com>
2023-06-13 18:01:02 +0200

    adding sameTree

--- Daily_Coding_Problem/Problem51.py
+++ Daily_Coding_Problem/Problem51.py
@@ -17,3 +17,22 @@
 # Example 2:
 # Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
 # Output: false
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, s, t):
+
+
+    def sameTree(self,s,t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return (self.sameTree(s.left,t.left) and self.sameTree(s.right,t.right))
+        
+        return 
