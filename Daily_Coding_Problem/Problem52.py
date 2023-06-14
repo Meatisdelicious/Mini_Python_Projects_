640df95707c6a46daad6736344d858dae45e217b
meatisdelicious <ssquerel@gmail.com>
2023-06-14 18:26:34 +0200

    adding new class

--- Daily_Coding_Problem/Problem52.py
+++ Daily_Coding_Problem/Problem52.py
@@ -13,3 +13,11 @@
 # Example 2:
 # Input: nums = [-7,-3,2,3,11]
 # Output: [4,9,9,49,121]
+
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
