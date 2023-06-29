2f552d33d62d373a52e491392780078eb0ae84c2
meatisdelicious <ssquerel@gmail.com>
2023-06-29 18:48:39 +0200

    problem solved but not understood

--- Daily_Coding_Problem/Problem60.py
+++ Daily_Coding_Problem/Problem60.py
@@ -20,3 +20,25 @@
 # Example 3:
 # Input: coins = [1], amount = 0
 # Output: 0
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        dp = [amount +1]*(amount+1)
+        dp[0]=0
+        for a in range(1,amount+1):
+            for c in coins:
+                if a -c >=0:
+                    dp[a] = min(dp[a],1+dp[a-c])
+                
+        return dp[amount] if dp[amount] != amount +1 else -1
+    
+coins = [1,2,5]
+amount = 11
+coins1 = [2]
+amount1 = 3 
+coins2 = [1]
+amount2 = 0   
+sol = Solution()
+sol.coinChange(coins, amount)
+
