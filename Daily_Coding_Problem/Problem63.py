4ae25022a2ebb807ce04fedfcd0a3c1a854b5094
meatisdelicious <ssquerel@gmail.com>
2023-07-05 18:01:14 +0200

    problem solved but not well understood

--- Daily_Coding_Problem/Problem63.py
+++ Daily_Coding_Problem/Problem63.py
@@ -3,10 +3,10 @@
 
 # Given an array of distinct integers candidates and a target integer target, 
 # return a list of all unique combinations of candidates where the chosen numbers
-#  sum to target. You may return the combinations in any order.
+# sum to target. You may return the combinations in any order.
 
 # The same number may be chosen from candidates an unlimited number of times.
-#  Two combinations are unique if the 
+#  Two combinations are unique if the  
 # frequency
 #  of at least one of the chosen numbers is different.
 
@@ -33,4 +33,19 @@
 
 class Solution(object):
     def combinationSum(self, candidates, target):
+        res = []
+
+
+        def dfs(i,cur,total):
+            if total == target:
+                return
+            if i>=len(candidates) or total >target:
+                return 
+            cur.append(candidates[i])
+            dfs(i,cur,total+candidates[i])
+            cur.pop()
+            dfs(i+1,cur,total)
+
+        dfs(0,[],0)
+
         return 
