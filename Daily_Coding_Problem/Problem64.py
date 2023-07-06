64eb2f797ecb1e3113cb0f40c2b914ab818058c4
meatisdelicious <ssquerel@gmail.com>
2023-07-06 17:45:34 +0200

    adding solution

--- Daily_Coding_Problem/Problem64.py
+++ Daily_Coding_Problem/Problem64.py
@@ -20,3 +20,16 @@
 # Output: [[1,2],[3,10],[12,16]]
 # Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res+intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0],newInterval[i]),max(newInterval[1],newInterval[i][1])]  
+        res.append(newInterval)  
+        return res
