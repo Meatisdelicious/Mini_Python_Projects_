e97ee11b2108afbe5cef3a4cfb58a5f40cae617b
meatisdelicious <ssquerel@gmail.com>
2023-07-10 17:54:13 +0200

    problem solved, still needs work

--- Daily_Coding_Problem/Problem65.py
+++ Daily_Coding_Problem/Problem65.py
@@ -26,11 +26,26 @@
 
 class Solution(object):
     def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        return
+        res = []
+        nums.sort()
+        for i,a in enumerate(nums):
+            # to not reuse the same value twice
+            if i>0 and a == nums[i-1]:
+                continue
+            l,r = i+1, len(nums)-1
+            # left and right can't be equal, so we just said that left<right
+            while l<r:
+                threeSum = a+nums[l] + nums[r]
+                if threeSum>0:
+                    r -=1
+                elif threeSum<0:
+                    l+=1
+                else:
+                    res.append([a,nums[l],nums[r]])
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+        return res
 nums1 = [-1,0,1,2,-1,-4]
 nums2 = [0,1,1]
 sol = Solution()
