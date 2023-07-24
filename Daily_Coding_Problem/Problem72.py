4769e3c9cb5992135f76476a2a098857ecac57f3
meatisdelicious <ssquerel@gmail.com>
2023-07-24 17:59:55 +0200

    problem solved and understood

--- Daily_Coding_Problem/Problem72.py
+++ Daily_Coding_Problem/Problem72.py
@@ -1,11 +1,9 @@
-# 128. Longest Consecutive Sequence
+# w8 128. Longest Consecutive Sequence
 
 # Given an unsorted array of integers nums,
 # return the length of the longest consecutive elements sequence.
 
 # You must write an algorithm that runs in O(n) time.
-
- 
 
 # Example 1:
 # Input: nums = [100,4,200,1,3,2]
@@ -15,3 +13,20 @@
 # Example 2:
 # Input: nums = [0,3,7,2,5,8,4,6,0,1]
 # Output: 9
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        numSet = set(nums)
+        longest = 0
+        for n in nums :
+            if (n-1) not in numSet:
+                length +=1
+                while (n + length ) in numSet:
+                    length +=1
+                longest = max(length,longest)
+        return longest
+
+nums = [100,4,200,1,3,2]
+nums2 = [0,3,7,2,5,8,4,6,0,1]
+sol = Solution()
+sol.longestConsecutive(nums)
