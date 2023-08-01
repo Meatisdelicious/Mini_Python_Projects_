a3c4cbd5f5b1d6697bc1fcb81faa58aa5ecaa40d
meatisdelicious <ssquerel@gmail.com>
2023-08-01 18:29:08 +0200

    adding comments

--- Daily_Coding_Problem/Problem74.py
+++ Daily_Coding_Problem/Problem74.py
@@ -16,5 +16,23 @@
 
 class Solution(object):
     def findMaxLength(self, nums):
+        seen_at ={}
+        seen_at[0]=-1
+        ans = count = 0
+        # enumerate is used to get the index and the value at the same time
+        for i,nums in enumerate(nums):
+            print("i :",i)
+            print("nums :",nums)
+            # if we get a count nums, add the count, else, add -1
+            count+=1 if nums else -1
+            if count in seen_at:
+                ans=max(ans,i-seen_at[count])
+            else :
+                seen_at[count]=i
+        return ans  
+    
+nums = [0,1]
+nums2 = [0,1,0]
 
-        return 
+sol = Solution()
+print(sol.findMaxLength(nums))
