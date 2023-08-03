720bd3b41103a89b242e8a04caa9f29471332702
meatisdelicious <ssquerel@gmail.com>
2023-08-03 18:02:53 +0200

    problem solved and partially understood

--- Daily_Coding_Problem/Problem75.py
+++ Daily_Coding_Problem/Problem75.py
@@ -5,13 +5,23 @@
 
 # A subarray is a contiguous non-empty sequence of elements within an array.
 
- 
-
 # Example 1:
-
 # Input: nums = [1,1,1], k = 2
 # Output: 2
-# Example 2:
 
+# Example 2:
 # Input: nums = [1,2,3], k = 3
 # Output: 2
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        res = 0
+        curSum=0
+        prefixSum = {0:1}
+        for n in nums:
+            curSum +=n
+            diff =curSum-k
+            res+=prefixSum.get(diff,0)
+            prefixSum[curSum]=1+prefixSum.get(curSum,0)
+
+        return res 
