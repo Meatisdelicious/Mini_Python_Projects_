e44eac6e557194ba667811c2a1fe534758e2a7c7
meatisdelicious <ssquerel@gmail.com>
2025-03-14 18:01:40 +0100

    day 8

---
+++ New_Daily_Coding_Problem/8_reverse_string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def wasa(self, s):
+        s.reverse()
+        return s
+
+
+s = ["h","e","l","l","o"]
+s1 = ["H","a","n","n","a","h"]
+sol=Solution()
+print(sol.wasa(s))
