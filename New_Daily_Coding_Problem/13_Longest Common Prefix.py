0807b34ccf36def9cb8acce01f96598963e219e5
meatisdelicious <ssquerel@gmail.com>
2025-03-27 18:57:20 +0100

    day 14, needed help on that one

--- New_Daily_Coding_Problem/13_Longest Common Prefix.py
+++ New_Daily_Coding_Problem/13_Longest Common Prefix.py
@@ -1,25 +1,19 @@
 class Solution(object):
-    def wasa(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        temp_list=[]
-        for word in strs:
-            print(word)
-            for i in range(len(word)):
-                print(word[i])
-            # for i in range(len(word)):
-            # for letter in word:
-            #     temp_list+=letter
-            #     if temp_list=
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        # Take the first string as a reference
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for s in strs[1:]:
+                # If current index is out of range or character doesn't match
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]
+        return strs[0]
 
-                # print(i)
-
-        return
     
 strs = ["flower","flow","flight"]
 strs1 = ["dog","racecar","car"]
 
 sol=Solution()
-print(sol.wasa(strs))
+print(sol.longestCommonPrefix(strs))
